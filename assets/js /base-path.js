e9fa1a69fb76550af1610f789d4bf2cd20820ce5
AndroGov-SA <amaghrabi@androomeda.com>
2026-01-12 12:40:12 +0300

    Delete assets directory

--- assets/js /base-path.js
+++
@@ -1,8 +0,0 @@
-// base-path.js
-// Auto-detect GitHub Pages base path (example: https://domain/AIT/...)
-(function () {
-  const parts = location.pathname.split("/").filter(Boolean);
-  // If URL is /AIT/admin/... => base = /AIT
-  window.BASE_PATH = parts.length ? `/${parts[0]}` : "";
-  window.withBase = (p) => `${window.BASE_PATH}${p}`;
-})();
